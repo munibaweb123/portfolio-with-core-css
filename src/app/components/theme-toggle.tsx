7e75583ee5918741e7de10fa3bfa5b18711cf4c3
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-24 20:07:28 +0500

    home/about page work

--- src/app/components/theme-toggle.tsx
+++ src/app/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 // components/ThemeToggle.js
 
 import { useEffect, useState } from 'react';
-import { FaSun, FaMoon } from 'react-icons/fa'; // Import sun and moon icons
+import { BsMoonFill } from 'react-icons/bs';  
 import { BsFillLightbulbFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
@@ -23,7 +23,7 @@ const ThemeToggle = () => {
 
     return (
         <button onClick={toggleTheme} style={{ display: 'flex', alignItems: 'center', background:'gold', border:'100px' }}>
-            {theme === 'light' ? <FaMoon size={40} color='black'/> : <BsFillLightbulbFill size={40} color='white' />} {/* Show moon icon for light, sun for dark */}
+            {theme === 'light' ? <BsMoonFill size={40} color='black'/> : <BsFillLightbulbFill size={40} color='white' />} {/* Show moon icon for light, sun for dark */}
             <span style={{ marginLeft: '8px' }}>
                 {theme === 'light' ? 'Dark' : 'Light'} 
             </span>
