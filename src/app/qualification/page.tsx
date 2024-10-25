65ebf2af30f48e940d7f36e4fabae29b7da6c03e
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-25 07:52:13 +0500

    screen settings

--- src/app/qualification/page.tsx
+++ src/app/qualification/page.tsx
@@ -4,13 +4,12 @@ import "../style/education.css"
 
 export default function Education(){
     return(
-        <div >
+        <div>
             <h2 className="heading">Education</h2>
             <div className="education">
             <Hero/>
             <Navbar/>
             </div>
-           
         </div>
     )
 }
