37d1746c3914fa9afa8f52055c5747b7660401b8
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-26 21:58:30 +0500

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,26 @@ export default function Contact(){
          <div className="contact">
            
             <Hero/>
-            <div className="container2"> <h2 className="heading">Contact Me</h2></div>
+            <div className="container2"> <h2 className="heading">Contact Me</h2>
+            <div className="sub-container">
+                <form className="form">
+                    <div>
+                        <label className="label">Email:</label><br/>
+                        <input className="input" type="email" />
+                    </div>
+                    <div>
+                        <label className="label">Subject:</label><br/>
+                        <input className="input" type="subject" />
+                    </div>
+                    <div>
+                        <label className="label">Message:</label><br/>
+                        <textarea className="input" typeof="message" rows={5}></textarea>
+
+                    </div>
+                    <button className="btn">Submit</button>
+                </form>
+            </div>
+            </div>
             <Navbar/>
         </div>
        </div>
